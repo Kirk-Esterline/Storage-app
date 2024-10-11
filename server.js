992f08c172897abea456195d65c920d0e38726b1
kirk-esterline <kirk.esterline@gmail.com>
2024-10-11 17:03:47 -0400

    still trying to connect and retrieve data

--- server.js
+++ server.js
@@ -17,3 +17,21 @@ const requestHandler = async (req, res) => {
 http.createServer(requestHandler).listen(3000, () => {
   console.log("Server running at http://localhost:3000");
 });
+
+
+async function testRun() {
+    try {
+        const post = await sql('SELECT * FROM playing_with_neon', [postID]);
+        if(!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+        console.log("It helps if you call the function")
+        console.log(post);
+    }
+    catch (error) {
+        console.error(error.message);
+    }
+}
+
+testRun();
+
