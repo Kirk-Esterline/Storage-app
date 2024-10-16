3d0ad565a435c0aec37ffc421ad617f8e73bdc9b
Kirk Esterline <kirk.esterline@gmail.com>
2024-10-16 16:56:28 -0400

    added controllers and user model

--- controllers/todos.js
+++ controllers/todos.js
@@ -11,5 +11,45 @@ module.exports = {
             console.log(err)
         }
     },
-    // More controllers to be added.
+    createTodo: async (req, res) => {
+        try{
+            await Todo.create({todo: req.body.todoItem, completed: false, userId: req.user.id})
+            console.log('Todo has been added!')
+            res.redirect('/todos')
+        }catch(err){
+            console.log(err)
+        }
+    },
+    markComplete: async (req, res) => {
+        try{
+            await Todo.findOneAndUpdate({_id:req.body.todoIdFromJSFile},{
+                completed: true
+            })
+            console.log('Marked Complete')
+            res.json('Marked Complete')
+        }catch(err){
+            console.log(err)
+        }
+    },
+    markIncomplete: async (req, res) => {
+        try{
+            await Todo.findOneAndUpdate({_id:req.body.todoIdFromJSFile},{
+                completed: false
+            })
+            console.log('Marked Incomplete')
+            res.json('Marked Incomplete')
+        }catch(err){
+            console.log(err)
+        }
+    },
+    deleteTodo: async (req, res) => {
+        console.log(req.body.todoIdFromJSFile)
+        try{
+            await Todo.findOneAndDelete({_id:req.body.todoIdFormJSFile})
+            console.log('Deleted Todo')
+            res.json('Deleted It')
+        }catch(err){
+            console.log(err)
+        }
+    }
 }
