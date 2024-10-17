331c046854d6f575df681d3f4dfee206410802c5
Kirk Esterline <kirk.esterline@gmail.com>
2024-10-17 16:50:17 -0400

    Filled out the rest of the basic application. Still needing to figure out MongoStore issues

--- config/passport.js
+++ config/passport.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose')
 const User = require('../models/User')
 
 module.exports = function (passport) {
-    passport.new(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+    passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
         User.findOne({ email: email.toLowerCase() }, (err, user) => {
             if (err) { return done(err) }
             if (!user) {
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express()
 const mongoose = require('mongoose')
 const passport = require('passport')
 const session = require('express-session')
-const MongoStore = require('connect-mongo')(session)
+const MongoStore = require('connect-mongo')
 const flash = require('express-flash')
 const logger = require('morgan')
 const connectDB = require('./config/database')
@@ -29,10 +29,10 @@ app.use(logger('dev'))
 // Sessions
 app.use(
     session({
-      secret: process.env.SECRET_SAUCE,
-      resave: false,
-      saveUninitialized: false,
-      store: new MongoStore({ mongooseConnection: mongoose.connection }),
+      secret: 'keyboard cat',
+      // resave: false,
+      // saveUninitialized: false,
+      store: MongoStore.create({ mongooseConnection: mongoose.connection }),
     })
 )
 
